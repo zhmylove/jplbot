b2d2175d88b2a53928f8a757931adb059f06a4cc
Sergey Zhmylove <zhmylove@narod.ru>
2016-01-10 23:40:55 +0300

    Add bomb command

--- src/jplbot.pl
+++ src/jplbot.pl
@@ -21,6 +21,13 @@ our $password = 'password';
 our $loop_sleep_time = 60;
 our $conference_server = 'conference.jabber.ru';
 our %forum_passwords = ('ubuntulinux' => 'ubuntu');
+our @colors = (
+   'бело-оранжевый', 'оранжевый',
+   'бело-зелёный', 'зелёный',
+   'бело-синий', 'синий',
+   'бело-коричневый', 'коричневый',
+);
+our $minimum_colors = 3;
 
 unless (my $ret = do './config.pl') {
    warn "couldn't parse config.pl: $@" if $@;
@@ -30,6 +37,12 @@ unless (my $ret = do './config.pl') {
 
 store {}, $karmafile unless -r $karmafile;
 my %karma = %{retrieve($karmafile)};
+my %bomb_time;
+my %bomb_correct;
+my %bomb_resourse;
+my %bomb_nick;
+srand;
+my $col_count = int($minimum_colors + ($#colors - $minimum_colors + 1) * rand);
 
 my $qname = quotemeta($name);
 $SIG{INT} = \&shutdown;
@@ -41,9 +54,23 @@ sub shutdown {
    exit 0;
 }
 
+sub bomb_user {
+   my ($bot, $user) = @_;
+   my $to = $bomb_resourse{lc($user)};
+   my $name = $bomb_nick{lc($user)};
+
+   delete $bomb_time{lc($user)};
+   delete $bomb_correct{lc($user)};
+   delete $bomb_resourse{lc($user)};
+   delete $bomb_nick{lc($user)};
+
+   $bot->SendGroupMessage($to, "$name: ты взорвался!");
+}
+
 sub background_checks {
    my $bot = shift;
    store \%karma, $karmafile;
+   foreach(keys %bomb_time){ bomb_user($bot, $_) if (time > $bomb_time{lc($_)} + 60); }
 }
 
 sub new_bot_message {
@@ -52,6 +79,9 @@ sub new_bot_message {
 
    my $from = $msg{'from_full'};
    $from =~ s{^.+/([^/]+)$}{$1};
+
+   my $resourse = $msg{'from_full'};
+   $resourse =~ s{^(.+)/[^/]+$}{$1};
 
    my $to_me = ($msg{'body'} =~ s{^$qname: }{});
 
@@ -96,7 +126,7 @@ sub new_bot_message {
 
       when (/^help\s*$/i) {
          $bot->SendGroupMessage($msg{'reply_to'},
-            "$from: пробуй так: date fortune karma time");
+            "$from: пробуй так: bomb date fortune karma time");
       }
 
       when (/^fortune\s*$/i) {
@@ -115,6 +145,50 @@ sub new_bot_message {
       when (/^karma\s*(\w+)$/i) {
          $bot->SendGroupMessage($msg{'reply_to'},
             "$from: карма $1: " . ($karma{lc($1)}||0));
+      }
+
+      when (/^bomb\s*$/i) {
+         $bot->SendGroupMessage($msg{'reply_to'},
+            "$from: бомба -- это не игрушки!");
+      }
+
+      when (/^bomb\s*(\w+)$/i) {
+         my $name = $1;
+
+         if (defined $bomb_time{lc($name)}) {
+            $bot->SendGroupMessage($msg{'reply_to'},
+               "$from: на $name уже установлена бомба.");
+            return;
+         }
+
+         my %selected_colors;
+         while($col_count != keys %selected_colors){
+            $selected_colors{$colors[int($#colors * rand)]} = 1;
+         }
+
+         my $selected_colors_t = join ', ', (sort keys %selected_colors);
+
+         $selected_colors_t =~ s/,( \S+)$/ и$1/i;
+
+         $bomb_time{lc($name)} = time;
+         $bomb_correct{lc($name)} = (keys %selected_colors)[0];
+         $bomb_resourse{lc($name)} = $resourse;
+         $bomb_nick{lc($name)} = $name;
+
+         my $txt;
+         if ($from eq $name) {
+            $txt = "$from, привык забавляться сам с собой?";
+         } else {
+            $txt = "Привет от $from, $name!";
+         }
+         $txt .= " Я хочу сыграть с тобой в игру.\n" .
+         "Правила очень простые. Всю свою жизнь ты не уважал random, " .
+         "и теперь пришло время поплатиться. \nНа тебе бомба, из которой торчат " .
+         "$selected_colors_t провода. \n" .
+         "Твоя задача -- правильно выбрать провод. " .
+         "До взрыва осталось 1-2 минуты. Время пошло!";
+
+         $bot->SendGroupMessage($msg{'reply_to'}, $txt);
       }
 
       when (/^(\w+):\s*\+[+1]\s*$/) {
@@ -189,7 +263,7 @@ sub new_bot_message {
                   }
 
                   $bot->SendGroupMessage($msg{'reply_to'},
-                     "$from: заголовок: $title");
+                     "$from: заголовок: [$title]");
                } else {
                   $bot->SendGroupMessage($msg{'reply_to'},
                      "$from: да ну нафиг это парсить...");
@@ -197,6 +271,21 @@ sub new_bot_message {
             });
 
          my $response = $ua->get($uri);
+      }
+
+      when (\@colors) {
+         return unless defined $bomb_correct{lc($from)};
+
+         if ($bomb_correct{lc($from)} eq $msg{'body'}) {
+            delete $bomb_time{lc($from)};
+            delete $bomb_correct{lc($from)};
+            delete $bomb_resourse{lc($from)};
+            delete $bomb_nick{lc($from)};
+            $bot->SendGroupMessage($msg{'reply_to'},
+               "$from, расслабься, это был всего-лишь розыгрыш!");
+         } else {
+            bomb_user($bot, $from);
+         }
       }
 
       default {
