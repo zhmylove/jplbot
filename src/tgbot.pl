75bca9d33cc6e90dc255576327c839e1dc028f51
Andrey Bova <andrewbova@yandex.ru>
2017-09-02 14:24:25 +0300

    Fix regex and unnecessary check
     - Add ignore case to regex
     - Remove unecessary undef check

--- src/tgbot.pl
+++ src/tgbot.pl
@@ -296,10 +296,11 @@ for(;;) {
             $tg->sendMessage({ chat_id => $chat, text => $joke });
          }
 
-         when (/^\s*(?:weather|погода)(?: (.*?))?\s*$/) {
+         when (/^\s*(?:weather|погода)(?:\s+(.*?))?\s*$/i) {
             my $chat = $upd->{message}{chat}{id};
-            my $city = $1 ? $1 : "Saint+Petersburg";
+            my $city = $1;
             my $url  = sweets->get_weather_image_url($city);
+            say $url;
             $tg->sendPhoto({ chat_id => $chat, photo => $url });
          }
       }
